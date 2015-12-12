ce31530ec4e5e71d6a09c2e6c87895b0ab76aa21
Eric M. David <ericdavid365@gmail.com>
2015-12-11 20:23:13 -0600

    Add valid moves for knight to move. TODO: bst of all possible moves?, bfs/dfs to end position

--- knight_travails.rb
+++ knight_travails.rb
@@ -1,76 +1,65 @@
 #create a chess board with a horizontal array and vertical area of possible moves
 class Board
-	attr_accessor :parent, :left_child, :right_child
-	attr_reader :value
-	def initialize(value, parent=nil, left_child=nil, right_child=nil)
-		@value = value
-		@parent = parent
-		@left_child = left_child if @left_child != nil
-		@right_child = right_child if @right_child != nil
+	attr_accessor :knight, :valid_moves
+	def initialize(knight, valid_moves=[])
+		@knight = knight
+		@valid_moves = valid_moves
+		validate_moves
 	end
 
 		# Put all instance variables of a board in an easily read string
 	def to_s
 		str = ""
-		str += "<board: #{@value} "
-		str += "@parent: #{@parent.value} " if !@parent.nil?
-		str += "@left_child: #{@left_child.value} " if !@left_child.nil?
-		str += "@right_child: #{@right_child.value}>" if !@right_child.nil?
-		str
+		str += "@knight: #{@knight} "
+		str += "@valid_moves: #{@valid_moves} "
 	end
 
-end
-
-def build_tree(array)
-	root = Board.new(array[0])
-	arr = Array.new
-	array.each_with_index do |num, index| 
-		index == 0 ?  arr << root : arr << link(root,num)
+	def possible_moves(knight_position)
+		possible_knight_moves = [[2,1],[2,-1],[1,2],[1,-2],[-2,1],[-2,-1],[-1,2],[-1,-2]]
+		possible_knight_moves.map! { |c| [c,knight_position].transpose.map { |t| t.reduce(:+) } }
 	end
-	arr
-end
 
-
-def link(board, num)
-	if num < board.value
-		if board.left_child.nil?
-			board.left_child = Board.new(num, board)
-		else
-			link(board.left_child, num)
+	def validate_moves
+		possible_moves(knight).each do |move|
+			if move.none? { |m| m > 7 || m < 0 }
+				@valid_moves << move
+			end
 		end
-	elsif num > board.value
-		if board.right_child.nil?
-			board.right_child = Board.new(num, board)
-		else
-			link(board.right_child, num)
-		end
-	else
-		return board
 	end
 end
 
-def bfs(array, value)
-	bst = build_tree(array)
+def build_tree(current_knight_position)
+	current_knight_position = Board.new(current_knight_position)
+	p current_knight_position.to_s
+	new_knight_position = 
+end
+
+def bfs(knight_start, knight_end)
+	bst = build_tree(knight_start)
 	queue = [bst.first]
 	until queue.empty?
 		current = queue.shift
-		return (p current.value) if current.value == value
-		queue << current.left_child if !current.left_child.nil?
-		queue << current.right_child if !current.right_child.nil?
+		return current if current.value == value
+		queue << current.left_child unless current.left_child.nil?
+		queue << current.right_child unless current.right_child.nil?
 	end
+	nil
 end
-#create a knight and place in a random location
+
+def dfs(array,value)
+	bst = build_tree(array)
+	stack = [bst.first]
+	until stack.empty?
+		current = stack.shift
+		return current if current.value == value
+		stack << current.left_child unless current.left_child.nil?
+		stack << current.right_child unless current.right_child.nil?
+	end
+	nil
+end
 #vertical and horizontal arrays act as children, root is knight location
 #create a bst to find this location
 # use dfs OR bfs to find the fastest way to get knight to specified location
+#create a knight and place in a random location
 
-def knight_travails(array)
-	knight = array.sample
-	knight
-end
-
-array = Array.new
-(0..8).each { |x| array << x }
-value = knight_travails(array)
-p array,value
-bfs(array, value)
+build_tree([3,7])
---
+++ test.rb
@@ -0,0 +1,36 @@
+class Node
+	attr_accessor :left, :right
+	attr_reader :desti
+	def initialize (desti, left = 0, right = 0)
+		@desti = desti
+		@left = left
+		@right = right
+		@left_goal = desti[0]
+		@right_goal = desti[1]
+		moves(@left, @left_goal, @right, @right_goal)
+	end
+
+	def moves(left, left_goal, right, right_goal)
+		until left == left_goal && right == right_goal
+			return (p "RIGHT ERROR") if (right < 0 || right > 7)
+			return (p"LEFT ERROR") if (left < 0 || left > 7)
+			queue = Array.new
+			queue << begin ([left + 1, right + 2]; [left + 1, right - 2]; [left + 2, right + 1]; [left + 2, right - 1]; [left - 2, right + 1]; [left - 2, right - 1]; [left -1, right + 2]; [left - 1, right - 2]) end
+			p queue
+			left = left_goal
+			right = right_goal
+		end
+		p left,left_goal,right,right_goal
+	end
+end
+
+
+	def possible_arrays(array)
+	possible_changes = [[2,1],[2,-1],[1,2],[1,-2],[-2,1],[-2,-1],[-1,2],[-1,-2]]
+	possible_changes.map! { |c| [c,array].transpose.map { |t| t.reduce(:+) } }
+	p possible_changes.sort
+	end
+	possible_arrays([3,3])
+
+#array = [(0..7).to_a.sample, (0..7).to_a.sample]
+#x = Node.new(array)
